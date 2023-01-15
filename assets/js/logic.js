98fdd1e58d5775ead20e36af6adc42493740d23e
Rania Benameur <raniabenameur97@gmail.com>
2023-01-15 22:21:47 +0000

    initial global variables

---
+++ assets/js/logic.js
@@ -0,0 +1,24 @@
+// Global variables
+var startButton = document.querySelector('#start');
+
+//console.log(startButton);
+
+var startScreenWrapper = document.querySelector('#start-screen');
+
+console.log(startScreenWrapper);
+
+//var questionWrapper = document.querySelector('#questions');
+
+console.log(questionWrapper);
+
+//var endScreenWrapper = document.querySelector('#end-screen');
+
+console.log(endScreenWrapper);
+
+//var submitButton = document.querySelector('#submit');
+
+console.log(submitButton);
+
+//var score = document.querySelector('#final-score');
+
+console.log(score);
